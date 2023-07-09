1bc5fc80b882981025b7e084370ac6ead61c7423
Artur <artur.chepurnykh@mail.ru>
2023-07-09 10:56:50 +0700

    Modules

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import {PHOTO_DESCRIPTIONS,MESSAGES,NAMES,PHOTOS_COUNT,getRandomInteger,createRandomIdFromRangeGenerator,createComment,createPhotoDescription} from './util.js';
-
+import {renderPictures} from './post.js';
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const createRandomDescriptionId = createRandomIdFromRangeGenerator(1, 25);
 const createRandomPhotoId = createRandomIdFromRangeGenerator(1, 25);
 const createRandomCommentId = createRandomIdFromRangeGenerator(1, 1000);
 const photos = Array.from({length: PHOTOS_COUNT}, createPhotoDescription);
+
+
